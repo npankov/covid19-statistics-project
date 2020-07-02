f39379a0458eb57faf8fe8a3828341428622c772
Nikolai <pankoff2000@gmail.com>
2020-07-02 17:17:14 +0200

    Updates

---
+++ precache-manifest.fc818e7a024fd4543ae3472fecd8b193.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69a939379b3094129e360df252ce51ca",
+    "url": "/covid19-statistic-project/index.html"
+  },
+  {
+    "revision": "83619b5a68043e7bff58",
+    "url": "/covid19-statistic-project/static/css/main.be199bf1.chunk.css"
+  },
+  {
+    "revision": "0cd9a2a1cde3169ebb44",
+    "url": "/covid19-statistic-project/static/js/2.2717b3ab.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-statistic-project/static/js/2.2717b3ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83619b5a68043e7bff58",
+    "url": "/covid19-statistic-project/static/js/main.2b667c1f.chunk.js"
+  },
+  {
+    "revision": "1d6ce75b32dbbd9156a2",
+    "url": "/covid19-statistic-project/static/js/runtime-main.832b3c43.js"
+  }
+]);
